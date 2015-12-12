4ef697e6b0695d94673683cf7b1955c4d9e1f71b
SavaIvanov9 <sava.ivanov.9@gmail.com>
2015-12-12 19:20:06 +0200

    Some starting test code
    Display objects

---
+++ MonoTest2/MonoTest2/SplashScreen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoTest2
+{
+    public class SplashScreen : GameScreen
+    {
+        Texture2D image;
+        string path;
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            path = "SplashScreen/CodeWizardHead";
+            image = content.Load<Texture2D>(path);
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(image, Vector2.Zero, Color.White);
+        }
+    }
+}
